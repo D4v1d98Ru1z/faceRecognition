5342b5d1390b06eb37e4b6c79d612e9c7dd58a20
davidlruizc <druiz@dbsolution.com.co>
2019-11-17 21:54:56 -0500

    rendering the data

--- web/face-detect-app/src/App.js
+++ web/face-detect-app/src/App.js
@@ -1,8 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const URLAPI = `http://localhost:5000`
+
 function App() {
+
+  // eslint-disable-next-line
+  const [data, setData] = useState([])
+  // eslint-disable-next-line
+  const [image, setImage] = useState('')
+
+  const handleClickImage = async() => {
+    try {
+      const testImage = `https://www.elsiglodetorreon.com.mx/m/i/2019/10/1235159.jpeg`
+      const fetchOptions = {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          image: testImage,
+        })
+      }
+
+      const resp = await fetch(`${URLAPI}/create-facelist`, fetchOptions)
+      const people = await resp.json()
+      console.log(people.data)
+      setData(people.data)
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -18,15 +49,32 @@ function App() {
             className="inputFile"
             placeholder="Upload image"
           />
-          <button className="buttonFile">Upload</button>
+          <button
+            className="buttonFile"
+            onClick={handleClickImage}
+          >
+            Upload
+          </button>
         </div>
+        <h3>Image attributes: </h3>
+        <ul>
+        {
+          data.map(item => (
+            <li key={item.faceId}>
+              <span>
+                Gender: {item.faceAttributes.gender}, age: {item.faceAttributes.age}
+              </span>
+            </li>
+          ))
+        }
+        </ul>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Watch the image
         </a>
       </header>
     </div>
